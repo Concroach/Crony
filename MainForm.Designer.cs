3fc5d7ce56f0d249788143518494cd9fec497a0b
unknown <merkoncel@gmail.com>
2024-10-03 11:17:58 +0300

    correct keyboardLocker button position

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -19,21 +19,22 @@
             this.btnToggleKeyboard = new System.Windows.Forms.Button();
             this.SuspendLayout();
             
-            // btnToggleKeyboard
-            this.btnToggleKeyboard.Location = new System.Drawing.Point(12, 12);
-            this.btnToggleKeyboard.Name = "btnToggleKeyboard";
-            this.btnToggleKeyboard.Size = new System.Drawing.Size(150, 23);
-            this.btnToggleKeyboard.TabIndex = 0;
-            this.btnToggleKeyboard.UseVisualStyleBackColor = true;
-            this.btnToggleKeyboard.Click += new System.EventHandler(this.btnToggleKeyboard_Click);
-            
             // MainForm
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(200, 100);
             this.Controls.Add(this.btnToggleKeyboard);
             this.Name = "MainForm";
             this.ResumeLayout(false);
+            
+            // btnToggleKeyboard
+            this.btnToggleKeyboard.Location = new System.Drawing.Point(25, 33);
+            this.btnToggleKeyboard.Name = "btnToggleKeyboard";
+            this.btnToggleKeyboard.Size = new System.Drawing.Size(150, 25);
+            this.btnToggleKeyboard.TabIndex = 0;
+            this.btnToggleKeyboard.UseVisualStyleBackColor = true;
+            this.btnToggleKeyboard.Click += new System.EventHandler(this.btnToggleKeyboard_Click);
+            
+            
         }
     }
 }
